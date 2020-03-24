481501281b0e18cb7511acac27c4a97b635908ef
Vicol Sergiu <sergiu.vicol@army.md>
2020-03-24 08:32:57 +0200

    Initial commit

---
+++ src/test/java/com/vicolexample/sfgthymeleaf/SfgThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicolexample.sfgthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
